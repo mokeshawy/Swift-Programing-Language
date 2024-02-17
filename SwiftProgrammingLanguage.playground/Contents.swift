1ce82b4ee596e03cceca3c411ec8dbbed6522898
mohamedkeshawy <mokeshawy@gmail.com>
2024-02-17 16:48:50 +0200

    Implement dictionary

--- SwiftProgrammingLanguage.playground/Contents.swift
+++ SwiftProgrammingLanguage.playground/Contents.swift
@@ -30,7 +30,7 @@ lastTime = "3:00 PM"
 
 /* ----------------- Arrays ------------------- */
 
-// array is data structure array cane be save mulitaples values from memory from one type and can be accessed to the element by "index" and first element from array start from index "0"
+// array is data structure array cane be save multiples values from memory from one type and can be accessed to the element by "index" and first element from array start from index "0"
 
 // cane be add new element after initialize array by "append"
 
@@ -44,12 +44,47 @@ var firstElement = arr[0]
 
 var arrayOfString = [String]() // empty array
 
-arrayOfString = ["E","F","G","O"] // initialize empty arry
-arrayOfString += ["P","Q","R","S"] // add multipale values into array with out using append
+arrayOfString = ["E","F","G","O"] // initialize empty array
+arrayOfString += ["P","Q","R","S"] // add multiple values into array with out using append
 
 arrayOfString.insert("U", at: 0) // add new value with specific index
 
-arrayOfString[0] = "T" // replace vlaue of index "0"
+arrayOfString[0] = "T" // replace value of index "0"
 
 
 arrayOfString.remove(at: 3) // remove value from array with specific
+
+var arrayCount = arr.count // get count of array
+
+
+/* ---------------- Dictionaries --------------*/
+
+// Dictionaries is a data structure we can store data with "Key and vlaue". But the dictionary does not care about the order of values
+
+// The key in dictionary unique
+
+var countries : [String : String] = ["EG": "Egypt","KSA":"Saudi Arabia","IRQ" : "Bagdad"]
+
+var emptyDictionary : [String : Int]
+emptyDictionary = ["one" : 1,  "two" : 2]
+
+var dictionaryAnyValues : [String : Any]
+dictionaryAnyValues = ["EGP" : "Egypt", "code" : 123 , "Address" : "Africa"]
+
+let Egypt = dictionaryAnyValues["EGP"] ?? ""
+
+// update on dictionary with "updateValue"
+dictionaryAnyValues.updateValue("Cairo", forKey: "EGP")
+// update on dictionary with "without updateValue"
+dictionaryAnyValues["EGP"] = "Naser City"
+
+// remove values in dictionary
+dictionaryAnyValues.removeValue(forKey: "EGP")
+
+
+// Dictionary count
+let dictionaryCount = dictionaryAnyValues.count
+
+print("Dictionary count = \(dictionaryCount)")
+
+
