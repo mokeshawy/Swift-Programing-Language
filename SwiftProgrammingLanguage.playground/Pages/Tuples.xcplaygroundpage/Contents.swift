bdf90539570f1e9d2545c83c25fc712ae806a2c4
mohamedkeshawy <mokeshawy@gmail.com>
2024-02-20 07:50:21 +0200

    Implement Tuples dataStructure

--- SwiftProgrammingLanguage.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
+++ SwiftProgrammingLanguage.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
@@ -1,7 +1,13 @@
-//: [Previous](@previous)
 
-import Foundation
+/* ----------- Tuples ---------- */
 
-var greeting = "Hello, playground"
+// The "Tuples" is a data structure Through it we can store different values on the same time.
 
-//: [Next](@next)
+
+// define "Tuples" with value only
+var userTuples = ("Mohamed Keshawy",36,"mokeshawy@gmail.com")
+print(userTuples.1)
+
+// define "Tuples" with key and value
+var userTuples1 = (name: "Mohamed", age: 36)
+print(userTuples1.name)
