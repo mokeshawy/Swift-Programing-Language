26d6ac9dcdb7f969b0eeed7ee424fc766e97e937
mohamedkeshawy <mokeshawy@gmail.com>
2024-02-23 18:55:03 +0200

    Implement  Closures

--- SwiftProgrammingLanguage.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ SwiftProgrammingLanguage.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 
 
@@ -149,5 +150,65 @@ func getNumber( num :Double) -> Double{
 func getNumber() -> Int{
     return 20
 }
+
+
+/* ------------- Closures ---------------- */
+
+// get odd and even numbers with out using Closures
+func getFilteredNumbers(arrNumbers: [Int]) -> ([Int],[Int]){
+    var oddNumbers = [Int]()
+    var evenNumbers = [Int]()
+    for num in arrNumbers {
+        if(num % 2 == 0){
+            oddNumbers.append(num)
+        }else{
+            evenNumbers.append(num)
+        }
+    }
+    return (oddNumbers,evenNumbers)
+}
+
+let arryNumbers = [2,4,5,7,8,12,13]
+
+print("odd and even numbers = \(getFilteredNumbers(arrNumbers: arryNumbers))")
+
+
+// get odd and even numbers using Closures
+func getFilteredNumbers(closures: (Int) -> Bool, arrNumbers: [Int]) -> [Int]{
+    var arrFilteredNumbers = [Int]()
+    for num in arrNumbers{
+        if closures(num){
+            arrFilteredNumbers.append(num)
+        }
+    }
+    return arrFilteredNumbers
+}
+
+let arr = [2,4,5,7,8,12,13]
+let oddNumbers = getFilteredNumbers(closures: { (num) -> Bool in getNumbersState(num1:num, num2:0)
+    
+},arrNumbers: arr )
+
+print("oddNumbers = \(oddNumbers)")
+
+let evenNumbers = getFilteredNumbers(closures: { (num) -> Bool in getNumbersState(num1:num, num2: 1)
+    
+}, arrNumbers: arr)
+
+print("evenNumbers = \(evenNumbers)")
+
+func getNumbersState(num1: Int,num2:Int) -> Bool{
+    return num1 % 2 == num2
+}
+
+func getNumberByClosures( numbers : (Int) -> Void ) {
+    numbers(8)
+}
+
+getNumberByClosures { num in
+   print("Numbers = \(num)")
+}
+
+
 
 
