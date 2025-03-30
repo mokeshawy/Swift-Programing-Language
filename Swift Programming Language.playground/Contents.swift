43af52681ad1dfdd519f3a2aa795ad08a19a62a6
mohamedkeshawy <mokeshawy@gmail.com>
2025-03-30 18:56:23 +0200

    add comment of var and let info

--- Swift Programming Language.playground/Contents.swift
+++ Swift Programming Language.playground/Contents.swift
@@ -2,8 +2,19 @@ import UIKit
 
             /* ---- Var vs Const ------ */
 
+/*
+ - immutable concept
+ - let cannot be changed value after declaration
+*/
 let name = "Mohamed"
+
+
+/*
+ - mutable concept
+ - var can be changed value after declration
+*/
 
 var age = 10
 
-print("Nmae: \(name) Agae: \(age)")
+
+
